74f5790d1c2b81cdcd1b44f66b608b89e6600da3
LiChen <2446432344@qq.com>
2023-03-14 23:27:05 +0800

    feat: 实现 isReactive 和 isReadonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,14 +1,21 @@
 import { track, trigger } from './effect';
+import { ReactiveFlags } from './reactive';
 
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true);
 
-function createGetter(isRaedonly = false) {
+function createGetter(isReadonly = false) {
   return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+
     const res = Reflect.get(target, key);
 
-    if (!isRaedonly) {
+    if (!isReadonly) {
       // 依赖收集
       track(target, key);
     }
