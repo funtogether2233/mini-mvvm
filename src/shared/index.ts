345c8671d80836d582c49a52a117be797e6ce1f7
LiChen <2446432344@qq.com>
2023-03-20 23:28:19 +0800

    feat: 实现组件 emit

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -14,3 +14,17 @@ export const isOn = (key) => /^on[A-Z]/.test(key);
 export function hasOwn(val, key) {
   return Object.prototype.hasOwnProperty.call(val, key);
 }
+
+// 首字母大写
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+// 添加 on 前缀，并且首字母大写
+export const toHandlerKey = (str: string) =>
+  str ? `on${capitalize(str)}` : ``;
+
+// 把烤肉串命名方式转换成驼峰命名方式
+const camelizeRE = /-(\w)/g;
+export const camelize = (str: string): string => {
+  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''));
+};
