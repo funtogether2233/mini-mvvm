43b3bd2b5d063fa7aa345f0e9b953b2e9e337580
LiChen <2446432344@qq.com>
2023-03-20 17:05:01 +0800

    feat: 初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,15 @@ import { h } from '../../lib/guide-mini-vue.esm.js';
 export const App = {
   // 必须要写 render
   render() {
-    return h('div', 'hi, ' + this.msg);
+    return h(
+      'div',
+      {
+        id: 'root',
+        class: ['red', 'hard']
+      },
+      // 'hi, ' + this.msg
+      [h('p', { class: 'red' }, 'hi'), h('p', { class: 'blue' }, 'mini-vue')]
+    );
   },
 
   setup() {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared/index';
 import { createComponentInstance, setupComponent } from './component';
 
 export function render(vnode, container) {
@@ -5,8 +6,44 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // 处理组件
-  processComponent(vnode, container);
+  // 判断 element
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    // 处理组件
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+
+  // string array
+  const { children } = vnode;
+  if (typeof children === 'string') {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    // vnode
+    mountChildren(vnode, el);
+  }
+
+  // props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode, container) {
