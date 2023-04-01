fde52ff62991a43eeaefd85b966b8b359f74307b
LiChen <2446432344@qq.com>
2023-04-01 16:47:14 +0800

    feat: 实现 transform

---
+++ src/compiler-core/transform.ts
@@ -0,0 +1,42 @@
+import { NodeTypes } from './ast';
+
+export function transform(root, options) {
+  const context = createTransformContext(root, options);
+
+  // dfs 遍历
+  traverseNode(root, context);
+
+  // 修改 text content
+}
+
+function createTransformContext(root, options) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  };
+
+  return context;
+}
+
+// dfs 遍历
+function traverseNode(node, context) {
+  console.log(node);
+
+  const { nodeTransforms } = context;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  traverseChildren(node, context);
+}
+
+function traverseChildren(node, context) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
